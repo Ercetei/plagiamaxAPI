72491e06d5889bd9916bffbd3dd96b9821ab1cc8
Ercetei <tournier.r@gmail.com>
2018-09-04 15:10:55 +0200

    #f77-2 - ajout contract

---
+++ src/main/java/com/infotel/plagiamax/contract/SeasonContract.java
@@ -0,0 +1,8 @@
+package com.infotel.plagiamax.contract;
+
+public class SeasonContract {
+	public static final String ID = "id";
+	public static final String START_DATE = "startDate";
+	public static final String END_DATE = "endDate";
+	public static final String STATUS = "status";
+}
