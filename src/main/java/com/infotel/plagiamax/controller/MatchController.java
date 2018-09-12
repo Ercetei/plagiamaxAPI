ca48d15fe7c5c4779673b84a5f9029f12b5541a7
Requistas <colin2.richard@gmail.com>
2018-09-12 15:17:31 +0200

    Résolution d'un problème dans le contrôleur match

--- src/main/java/com/infotel/plagiamax/controller/MatchController.java
+++ src/main/java/com/infotel/plagiamax/controller/MatchController.java
@@ -1,50 +1,16 @@
 package com.infotel.plagiamax.controller;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.infotel.plagiamax.controller.base.BaseRestController;
 import com.infotel.plagiamax.model.Match;
-import com.infotel.plagiamax.model.MatchTeam;
-import com.infotel.plagiamax.repository.MatchCrudRepository;
-import com.infotel.plagiamax.repository.MatchTeamCrudRepository;
-import com.infotel.plagiamax.repository.TeamCrudRepository;
 
 @RestController
 @RequestMapping(MatchController.BASE_URL)
 public class MatchController extends BaseRestController<Match, Long> {
 
 	public static final String BASE_URL = "/match";
-
-	@Autowired
-	private MatchCrudRepository matchCrudRepository;
-	
-	@Autowired
-	private MatchTeamCrudRepository matchTeamCrudRepository;
-	
-	@Autowired
-	private TeamCrudRepository teamCrudRepository;
-
-	@RequestMapping("/{index}/teams")
-	public ResponseEntity<Optional<Match>> getTeamsByIndex(@PathVariable("index") Long index) {
-		Optional<Match> item = matchCrudRepository.findById(index);
-		item.ifPresent(match -> {
-			match.setMatchteams(matchTeamCrudRepository.findByMatchId(match.getId()));
-			
-			for(MatchTeam matchTeam : match.getMatchteams()) {
-				matchTeam.setTeam(teamCrudRepository.findById(matchTeam.getTeam().getId()).get());
-			}
-		});
-		
-		new ResponseEntity<Match>(HttpStatus.OK);
-		return ResponseEntity.ok(item);
-	}
 	
 	/*item.ifPresent(match -> {
 		match.setMatchteams(matchTeamCrudRepository.findByMatchId(match.getId()));
