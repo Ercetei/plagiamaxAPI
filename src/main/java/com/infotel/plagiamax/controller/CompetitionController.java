5bd920e7c2d24b04606f39206dbd1b4a511792d2
MarineDijoux <dijoux.marine@laposte.net>
2018-09-18 18:04:28 +0200

    get matchday by competition

--- src/main/java/com/infotel/plagiamax/controller/CompetitionController.java
+++ src/main/java/com/infotel/plagiamax/controller/CompetitionController.java
@@ -1,14 +1,32 @@
 package com.infotel.plagiamax.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.infotel.plagiamax.controller.base.BaseRestController;
+import com.infotel.plagiamax.model.MatchDay;
 import com.infotel.plagiamax.model.Competition;
+import com.infotel.plagiamax.repository.MatchDayCrudRepository;
 
 @RestController
 @RequestMapping(CompetitionController.BASE_URL)
 public class CompetitionController extends BaseRestController<Competition, Long> {
 
     public static final String BASE_URL = "/competition";
+    
+	@Autowired
+	private MatchDayCrudRepository matchDayCrud;
+	
+	@RequestMapping(path = { "/{index}/matchdays" }, method = RequestMethod.GET)
+	public ResponseEntity<Iterable<MatchDay>> getMatchDayByCompetition(@PathVariable("index") Long index) {
+		Iterable<MatchDay> item = matchDayCrud.findByCompetitionId(index);
+		new ResponseEntity<Competition>(HttpStatus.OK);
+		return ResponseEntity.ok(item);
+	}
+    
 }
--- src/main/java/com/infotel/plagiamax/repository/MatchDayCrudRepository.java
+++ src/main/java/com/infotel/plagiamax/repository/MatchDayCrudRepository.java
@@ -1,5 +1,8 @@
 package com.infotel.plagiamax.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.infotel.plagiamax.model.MatchDay;
@@ -7,5 +10,9 @@ import com.infotel.plagiamax.repository.base.IBaseRepository;
 
 @Repository
 public interface MatchDayCrudRepository extends IBaseRepository<MatchDay, Long> {
-
+	
+	@Query(value = ("SELECT md FROM MatchDay md INNER JOIN Season s ON md.season.id = s.id "
+			+ "INNER JOIN Competition cn ON s.competition.id = cn.id WHERE cn.id = ?1"))
+	List<MatchDay> findByCompetitionId(Long competition_id);
+	
 }
