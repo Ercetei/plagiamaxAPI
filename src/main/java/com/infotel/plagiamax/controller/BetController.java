5d630b6626a009402b96b0655a4edb68df453e01
Ercetei <tournier.r@gmail.com>
2018-09-21 11:36:15 +0200

    correctif security config + appel spécifique team by bettype

--- src/main/java/com/infotel/plagiamax/controller/BetController.java
+++ src/main/java/com/infotel/plagiamax/controller/BetController.java
@@ -22,6 +22,6 @@ public class BetController extends BaseRestController<Bet, Long>{
 	/** The bet crud repository. */
 	@Autowired
 	BetCrudRepository betCrudRepository;
-	
+
 }
 
--- src/main/java/com/infotel/plagiamax/repository/TeamCrudRepository.java
+++ src/main/java/com/infotel/plagiamax/repository/TeamCrudRepository.java
@@ -1,5 +1,7 @@
 package com.infotel.plagiamax.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +16,22 @@ public interface TeamCrudRepository extends IBaseRepository<Team, Long>{
 	
 	@Query(value = ("SELECT COUNT(ev) FROM Event ev WHERE ev.team.id = ?1 AND ev.match.id = ?2 AND status = 1"))
 	Integer getTeamScoreByMatch(Long team_id, Long match_id);
+	
+/*	@Query(value = ("SELECT t FROM Bet b "
+			+ "INNER JOIN BetLine bl ON bl.bet.id = b.id "
+			+ "INNER JOIN BetType bt ON bl.bettype.id = bt.id "
+			+ "INNER JOIN MatchBet mb ON mb.id = bt.id "
+			+ "INNER JOIN Match m ON m.id = mb.match.id "
+			+ "INNER JOIN MatchTeam mt ON mt.match.id = m.id "
+			+ "INNER JOIN Team t ON t.id = mt.team.id "
+			+ "WHERE b.id = ?1"))
+	List<Team> getTeamsByBetId(Long bet_id);*/
+	
+	@Query(value = ("SELECT t FROM BetType bt "
+			+ "INNER JOIN MatchBet mb ON mb.id = bt.id "
+			+ "INNER JOIN Match m ON m.id = mb.match.id "
+			+ "INNER JOIN MatchTeam mt ON mt.match.id = m.id "
+			+ "INNER JOIN Team t ON t.id = mt.team.id "
+			+ "WHERE bt.id = ?1"))
+	List<Team> getTeamsByBettypeId(Long bet_id);
 }
